477e4132102315394f4edc9b7f57f5fda5661e77
mkumar <mkumar@synamedia.com>
2021-08-17 23:52:38 +0530

    Day 6 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -10,7 +10,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
-
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -59,23 +59,37 @@ class _LoginPageState extends State<LoginPage> {
                     ),
 
                     InkWell(
-                      onTap: () {
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoutes);
                       },
-                      child: Container(
-                        width: 150,
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
                         height: 50,
                         alignment: Alignment.center,
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
                         decoration: BoxDecoration(
                           color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(10),
+                          shape: (changeButton
+                              ? BoxShape.circle
+                              : BoxShape.rectangle),
+                          // borderRadius:
+                          //     BorderRadius.circular(changeButton ? 20 : 10),
                         ),
                       ),
                     )
